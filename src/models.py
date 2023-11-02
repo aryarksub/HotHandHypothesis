7c98ad9de141ab65317baa44925f10f66425b065
Arya K <aryarksub@gmail.com>
2023-11-02 00:25:24 -0700

    Add polynomial logistic regression model

--- src/models.py
+++ src/models.py
@@ -1,13 +1,14 @@
 from itertools import combinations
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 from sklearn.linear_model import LogisticRegression
 
 from data_processor import get_cleaned_shot_data
 
-def select_in_features(df, all_in_features, out_features, max_in_features=None, verbose=False):
+def select_in_features(model, df, all_in_features, out_features, max_in_features=None, verbose=False):
     best_score = (0, [])
-    lr_model = LogisticRegression()
     kfold_cv = KFold(n_splits=10)
 
     out_values = df[out_features].values.ravel()
@@ -19,7 +20,7 @@ def select_in_features(df, all_in_features, out_features, max_in_features=None, 
         all_in_feature_combinations = combinations(all_in_features, num_features)
         for in_features in all_in_feature_combinations:
             in_features = list(in_features)
-            scores = cross_val_score(lr_model, df[in_features].values, out_values, cv=kfold_cv)
+            scores = cross_val_score(model, df[in_features].values, out_values, cv=kfold_cv)
             accuracy = np.mean(scores)
             if accuracy > best_score[0]:
                 best_score = (accuracy, in_features)
@@ -36,17 +37,33 @@ def logistic_regression(df, in_features, out_features, verbose=False):
         print(f"Accuracy: {clf_lr.score(X_test, y_test)}")
     return clf_lr
 
-df = get_cleaned_shot_data()
+def poly_log_model(degree):
+    poly_model = PolynomialFeatures(degree=degree, interaction_only=False, include_bias=True)
+    lr_model = LogisticRegression()
+    return Pipeline([('poly_model', poly_model), ('log_reg_model', lr_model)])
 
-in_features = ["SHOT_CLOCK", "DRIBBLES", "SHOT_DIST", "CLOSE_DEF_DIST", "TOT_TIME_LEFT", "GAME_CLOCK"]
-out_features = ["FGM"]
+def polynomial_logistic_regression(df, in_features, out_features, degree=2, verbose=False):
+    pipeline = poly_log_model(degree)
+    
+    X_train, X_test, y_train, y_test = train_test_split(df[in_features], df[out_features], train_size=0.75)
+    pipeline.fit(X_train, y_train.values.ravel())
+    if verbose:
+        print(f"Accuracy: {pipeline.score(X_test, y_test)}")
+    return pipeline
 
-### Uncomment to get best features
-# best_score, best_in_features = select_in_features(df, in_features, out_features, verbose=True)
+if __name__ == "__main__":
+    df = get_cleaned_shot_data()
 
-best_in_features = ['DRIBBLES', 'SHOT_DIST', 'CLOSE_DEF_DIST', 'TOT_TIME_LEFT', 'GAME_CLOCK']
+    in_features = ["SHOT_CLOCK", "DRIBBLES", "SHOT_DIST", "CLOSE_DEF_DIST", "TOT_TIME_LEFT", "GAME_CLOCK"]
+    out_features = ["FGM"]
 
-lr_model = logistic_regression(df, best_in_features, out_features, verbose=True)
-print(lr_model.coef_)
+    ### Uncomment to get best features
+    # best_score, best_in_features = select_in_features(LogisticRegression(), df, in_features, out_features, verbose=True)
 
+    best_in_features = ['DRIBBLES', 'SHOT_DIST', 'CLOSE_DEF_DIST']
 
+    lr_model = logistic_regression(df, best_in_features, out_features, verbose=True)
+    print(lr_model.coef_)
+
+    poly_lr_model = polynomial_logistic_regression(df, best_in_features, out_features, verbose=True)
+    print(poly_lr_model[1].coef_)
