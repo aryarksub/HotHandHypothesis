0e853f89a9d1769bfec2db935ec9c1c284d5be3b
Arya K <aryarksub@gmail.com>
2023-11-04 20:53:16 -0700

    Add evaluation method for basic logistic regression model

---
+++ src/model_eval.py
@@ -0,0 +1,47 @@
+import numpy as np
+from itertools import combinations, product
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+from data_processor import get_cleaned_shot_data
+
+def evaluate_model(model, df, in_features, out_features, verbose=False):
+    kfold_cv = KFold(n_splits=10)
+    out_values = df[out_features].values.ravel()
+    scores = cross_val_score(model, df[in_features].values, out_values, cv=kfold_cv)
+    accuracy = np.mean(scores)
+    if verbose:
+        print(f"Accuracy: {accuracy}")
+    return accuracy
+
+def best_features_for_model(model, df, model_name=None, verbose=False):
+    if verbose and model_name is not None:
+        print(f"Determining best features for model: {model_name}")
+
+    # keywords for feature columns that are most important for shot quality
+    keywords = ["DRIBBLES", "SHOT_DIST", "CLOSE_DEF_DIST", "HEIGHT"]
+    all_columns = set(df.columns)
+    # separate feature columns based on keywords
+    columns = {keyword : [col for col in all_columns if keyword in col] for keyword in keywords}
+
+    out_features = ["FGM"]
+
+    best_score, best_features = 0, []
+
+    # choose one feature column from each keyword
+    for feature_list in product(*list(columns.values())):
+        score = evaluate_model(model, df, list(feature_list), out_features, verbose=False)
+        if score > best_score:
+            best_score = score
+            best_features = feature_list
+            if verbose:
+                print(f"New best accuracy ({round(best_score, 5)}) with features: {best_features}")
+    return best_score, best_features
+
+if __name__ == '__main__':
+    df = get_cleaned_shot_data()
+
+    log_reg_accuracy, log_reg_features = best_features_for_model(
+        LogisticRegression(max_iter=400), df, model_name="Logistic regression", verbose=True
+    )
+    print(f"Best logistic regression model:\n  Features: {log_reg_features}\n  Accuracy: {log_reg_accuracy}")
