bd4f00a3cdbd3fb7efb2acf5774865e32bd9a3dd
Arya K <aryarksub@gmail.com>
2023-12-22 15:47:19 -0800

    Change fgpct columns in shot result dataset to num shots made

--- src/shot_results.py
+++ src/shot_results.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 
 def add_previous_shot_results(df, num_shots=5):
-    # Add result of shot taken i shots ago (1 <= i <= num_shots)
+    # Result of shot taken i shots ago (1 <= i <= num_shots)
     for shot in range(1, num_shots+1):
         df[f"FGM-{shot}"] = np.where(df["SHOT_NUMBER"] >= shot+1, df["FGM"].shift(shot), 0).astype(np.int8)
         
-    # Add pct of last i shots (1 <= i <= num_shots)
+    # Num of shots made in last i shots (1 <= i <= num_shots)
     for shot in range(1, num_shots+1):
-        df[f"FGPCT-{shot}"] = np.where(df["SHOT_NUMBER"] >= shot+1, sum(df[f"FGM-{i}"] for i in range(1, shot+1)) / shot, None)
+        df[f"TOT_FGM-{shot}"] = np.where(df["SHOT_NUMBER"] >= shot+1, sum(df[f"FGM-{i}"] for i in range(1, shot+1)), None)
 
-    # Add results of last i shots as a string (1 <= i <= num_shots)
+    # Results of last i shots as a string (1 <= i <= num_shots)
     # E.g. "01101" means that shot n-1 missed, n-2 made, n-3 made, n-4 missed, n-5 made
     for shot in range(1, num_shots+1):
         def concatenate_results(row):
